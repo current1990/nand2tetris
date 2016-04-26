dbd1fd4c5bfa068a49cd3257ab5457261f351466
current1990 <wutao901225@foxmail.com>
2016-04-27 01:32:25 +0800

    Finish Project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -9,3 +9,79 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+// 256 * 512 / 16 = 8192
+// Staring from 0 so offset is in [0, 8192)
+
+(MAINLOOP)
+@KBD
+D=M
+
+@CLEAR
+D; JEQ
+
+@FILL
+D; JNE
+
+@MAINLOOP
+D; JMP
+
+(CLEAR)
+//Set initial offset to 8191
+@8191
+D=A
+
+@offset
+M=D
+
+(CLRLOOP)
+@offset
+D=M
+
+// Set M[SCREEN+OFFSET] = 0
+@SCREEN
+D=D+A
+// A = SCREEN + offset
+A=D
+M=0
+
+// offset--
+@offset
+M=M-1
+D=M
+
+@CLRLOOP
+D; JGE
+
+// Return
+@MAINLOOP
+D; JMP
+
+
+(FILL)
+//Set initial offset to 8191
+@8191
+D=A
+
+@offset
+M=D
+
+(FILLLOOP)
+@offset
+D=M
+
+@SCREEN
+D=D+A
+A=D
+M=-1
+
+@offset
+M=M-1
+D=M
+
+
+@FILLLOOP
+D; JGE
+
+@MAINLOOP
+D; JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,75 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// i = lhs(R0)
+@R0
+D=M
+
+@i
+M=D
+
+// j = rhs(R1)
+@R1
+D=M
+
+@j
+M=D
+
+// M[R2] = 0
+@R0
+D=A
+
+@R2
+M=D
+
+// Multi
+(LOOP)
+
+// if lhs == 0, GOTO END
+@i
+D=M
+
+@END
+D; JEQ
+
+@ADD
+D; JGT
+
+@MINUS
+D; JLT
+
+(ADD)
+// R2 = R2 + j
+@j
+D=M
+
+@R2
+M=M+D
+
+//i = i - 1
+@i
+M=M-1
+
+@LOOP
+M; JMP
+
+(MINUS)
+// R2 = R2 - j
+@j
+D=M
+
+@R2
+M=M-D
+
+//i = i + 1
+@i
+M=M+1
+
+@LOOP
+M; JMP
+
+(END)
+@END
+0; JEQ
